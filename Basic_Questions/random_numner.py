8deb5250f9ea22120c8b83cfb6d26cd9b1f57c81
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-30 12:17:24 +0300

    Selecting random numbers

---
+++ Basic_Questions/random_numner.py
@@ -0,0 +1,11 @@
+# Picking a random number x, and raising in to the power of another random number y
+# Pick the random number by importing the random function
+import random
+x = random.randint(2, 23)
+y = random.randint(2, 23)
+# Raise x to y as POWER
+POWER = x**y
+# Multiply power with pi, 3.14, as area of the circle
+pi = 3.14
+area = POWER * pi
+print(area)
