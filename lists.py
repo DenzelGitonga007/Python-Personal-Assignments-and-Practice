0010cdf6bbeca2a54d337ea9e48ef730fc7f2553
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-03 21:36:26 +0300

    Manually replacing the contents of a list

--- lists.py
+++ lists.py
@@ -5,3 +5,9 @@ HOD = ["Mr. Ayub Shirandula"]
 # printing the employee details
 print("Name: {} | Year of Birth: {} | Country: {}".format(employee[0], employee[1], employee[2])) #using the format method
 print("In the deparment: %s"%(Dep[0])) # Using the %s
+
+# Replacing an entry on the list
+cars = ["Vitz", "Mazda", "Mercedes"] # Original list
+print("This is the original list: {}".format(cars))
+cars[2] = "Demio" #Replace
+print("The list after adding an entity: %s"%(cars[:]))
