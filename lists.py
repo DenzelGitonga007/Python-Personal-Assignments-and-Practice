0941850e0a8a40e50931f0669e6776b2a1953a6a
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-03 21:19:33 +0300

    Lists

---
+++ lists.py
@@ -0,0 +1,7 @@
+# To output employee's, departments, and HOD's details
+employee = ["Denzel Gitonga", 2002, "Kenya"]
+Dep = ["ICT Support"]
+HOD = ["Mr. Ayub Shirandula"]
+# printing the employee details
+print("Name: {} | Year of Birth: {} | Country: {}".format(employee[0], employee[1], employee[2])) #using the format method
+print("In the deparment: %s"%(Dep[0])) # Using the %s
