b326120c362329d111913a620477be4e22534892
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-06 09:41:41 +0300

    sequence datatypes

---
+++ Basic_Questions/sequence_datatypes.py
@@ -0,0 +1,10 @@
+# To display the different sequence data_types
+a = [] #this should be a list
+b = {} #ought to be a dictionary
+c = set() #a set
+d = () # tuple
+# printing each one
+print("This is an empty " + str(type(a))) #list
+print("This is an empty " + str(type(b))) #dictionary
+print("This is an empty " + str(type(c))) #set
+print("This is an empty " + str(type(d))) #tuple
