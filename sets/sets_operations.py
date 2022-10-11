1294b5c4e831734ffa474f316dca2ca71de8ba25
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-11 11:05:26 +0300

    set union

---
+++ sets/sets_operations.py
@@ -0,0 +1,8 @@
+# Set operations like Union, Intersection, and set differences
+# set Union
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+# confirming that a and b are actual sets
+print("a and b are both {}".format(type(a)))
+print("Set a = {} and set b = {}".format(a, b))
+print("Set union: " + str(a | b))
