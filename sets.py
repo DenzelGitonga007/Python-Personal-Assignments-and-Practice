f8d36836272c244ad8975fcc67bd518d68039202
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-11 10:53:38 +0300

    Modifying the sets

--- sets.py
+++ sets.py
@@ -1,7 +1,9 @@
 # Difference between a set and a dictionary is, a dictionary has a key and an associate value
-numbers = {1, 2, 3}
+numbers = {1, 2, 3} #this is a set
+name = set("Denzel") #this is also a set that'll split the contents into one by one
 alpha_numeric = {1:20, 20:2} #The key and the associate value
 print(type(numbers))
+print(type(name))
 print(type(alpha_numeric))
 
 # Initializing a set
@@ -18,3 +20,10 @@ values.discard(4)
 print("Discarded an element " + str(values))
 values.remove(5)
 print("Removed an element " + str(values))
+# A string in set
+name = set("Denzel")
+print(name) 
+print ("The item removed/popped is " + str(name.pop()))#pops a random number
+print(name)
+
+
