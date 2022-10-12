f1a37f6abc20aefb5cbe511910ba7dd1684cdc0c
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-12 15:13:17 +0300

    returning more than one value

--- tuples.py
+++ tuples.py
@@ -5,3 +5,11 @@ name = zip(first_name, last_name)
 print(name) #initiates the zipped tuple
 people = tuple(name)
 print(people)
+# Returning more than one value using tuples
+def mean(x, y, z):
+    sum = x + y + z
+    avg = sum/3
+    return (sum, avg)
+(s, a) = mean(27, 30, 35)
+print("The sum is: " + str(s))
+print("The average is: " + str(a))
