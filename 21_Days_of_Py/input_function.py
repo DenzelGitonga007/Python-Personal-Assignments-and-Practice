99b964789a6884f382e62fe480bd5aa399eb4a63
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-13 12:13:59 +0300

    Day 1 of 21, using the input function

---
+++ 21_Days_of_Py/input_function.py
@@ -0,0 +1,5 @@
+#Day 1
+# Taking first and last name inputs and returning them
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hi there {} {}".format(first_name, last_name))
