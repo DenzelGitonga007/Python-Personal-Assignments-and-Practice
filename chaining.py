5d570900a41411a730cf5330d31684de4fa493ea
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-16 20:24:33 +0300

    chaining in python

---
+++ chaining.py
@@ -0,0 +1,6 @@
+# chaining in comparison operators
+print("...Compare different numbers(3)...")
+x = input("Please enter a number: ")
+y = input("Enter a second one: ")
+z = input("And the now the last one: ")
+print(x < y < z)
