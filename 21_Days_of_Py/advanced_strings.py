c589f0dde304ec502850709c507e7766d42e9ff4
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-17 19:06:53 +0300

    advanced strings

--- 21_Days_of_Py/advanced_strings.py
+++ 21_Days_of_Py/advanced_strings.py
@@ -2,4 +2,9 @@
 # and last characters
 string_one = input("Enter a name, any name: ")
 string_two = input("Enter a second one: ")
-print("{} {}".format(string_one, string_two))
+print(string_one[0] + string_two[0] + 
+        string_one[int(len(string_one)/2)] + string_two[int(len(string_two)/2)] + 
+        string_one[-1] + string_two[-1]
+    )
+
+
