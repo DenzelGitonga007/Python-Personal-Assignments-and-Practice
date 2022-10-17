50dcfd4026855fa96af126f660fdfc7ee31c11fc
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-17 11:05:55 +0300

    advanced strings

---
+++ 21_Days_of_Py/advanced_strings.py
@@ -0,0 +1,5 @@
+# Initializing strings and printing the first, middle,
+# and last characters
+string_one = input("Enter a name, any name: ")
+string_two = input("Enter a second one: ")
+print("{} {}".format(string_one, string_two))
