cd62481a48a30ca5b7e2ab08247fa09a8cb61c88
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-21 09:53:58 +0300

    180 degrees turn

--- 21_Days_of_Py/print_iteration.py
+++ 21_Days_of_Py/print_iteration.py
@@ -4,6 +4,20 @@ limit = int(input("How many stars do you wish to see? "))
 for rows in range(0, limit): #assign the rows
     for columns in range(0, rows+1): #assign the columns to iterate (rows += 1)
         # print the stars
-        print("* ", end="")
+        print("* ", end=" ")
     # Print each on a new row
     print("\r")
+
+#Trying a 180 degrees turn
+print(" ")
+print("...180 degrees turn...")
+print(" ")
+row_limit = limit
+column_limit = limit
+for row_num in range(0, row_limit+1): #assigning the rows
+    for column_num in range(column_limit-row_num, 0, -1):
+        # Print the stars
+        print("* ",end=" ")
+    # Print each on new rows
+    print("\r")
+
