8b1b9ba21e72a5d15182d38123f80c68d97f0d15
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-24 21:55:14 +0300

    dates with error handling

--- 21_Days_of_Py/date_day.py
+++ 21_Days_of_Py/date_day.py
@@ -3,9 +3,17 @@ print("...Get the day corresponding to a certain date...")
 print(" ")
 from datetime import datetime
 # Define the variables/parameters
-a = int(input("Which year was it? "))
-b = int(input("The month (in numbers)? "))
-c = int(input("The day of the month? "))
+#Capture only the integers
+while True:
+    # For the year
+    try:
+        a = int(input("Which year was it? "))
+        b = int(input("The month (in numbers)? "))
+        c = int(input("The day of the month? "))
+        break
+    except:
+        print("Please enter valid dates:")
+# The variable holding the date
 set_date = datetime(a, b, c)
 print("The date {} was on {}".format(set_date.strftime("%Y-%m-%d"), set_date.strftime("%A")))
 # print(set_date.strftime("%A"))
