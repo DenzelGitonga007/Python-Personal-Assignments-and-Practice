7df8454001f2024d14c857be51b90faf155d127a
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-27 04:35:31 +0300

    play music

---
+++ Junior Programs/playsound_prog.py
@@ -0,0 +1,6 @@
+# Playing sound
+# pip install playsound
+from playsound import playsound
+music = "C:\\Users\\Denzel\\Music\\Bali Nashukuru By Dr. Epiana.mp3"
+print("Playing {}".format(music))
+playsound(music)
