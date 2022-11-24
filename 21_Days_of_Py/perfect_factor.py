7bb644c6a6c0fd4b25dedf54c0901dbeb66e453e
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-24 11:32:19 +0300

    perfect number

--- 21_Days_of_Py/perfect_factor.py
+++ 21_Days_of_Py/perfect_factor.py
@@ -9,3 +9,23 @@
 # 1 + 5 = 6 therefore n = 25 is not a perfect number, so you should return False.
 
 print("...Finding a perfect number...")
+# Get the number from the user
+test_num = int(input("Enter any number: "))
+print("The factors of {} are: ".format(test_num))
+for values in range(1, test_num):
+    # Obtain only factors-- numbers that can divide 28 without a remainder 
+    factors = 0
+    if test_num % values == 0:
+        factors = values
+        print(factors)
+# Adding the factors to check that if they form a perfect number
+sum_of_factors = sum(
+    accurate_factors for accurate_factors in range(1, test_num) 
+    if test_num % accurate_factors == 0
+    )
+# print(sum_of_factors)
+# printing whether the test_num is a perfect number or not
+if sum_of_factors == test_num:
+    print("The sum of the factors is '{}', hence, '{}' is a perfect number".format(sum_of_factors, test_num))
+else:
+    print("The sum of the factors is '{}', hence, '{}' is not a perfect number".format(sum_of_factors, test_num))
