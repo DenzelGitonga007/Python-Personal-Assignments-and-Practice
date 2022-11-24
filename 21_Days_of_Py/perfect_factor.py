5ce6db5552aef3d999dd5399d027535447caaf8f
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-24 10:42:17 +0300

    finding a perfect number

---
+++ 21_Days_of_Py/perfect_factor.py
@@ -0,0 +1,11 @@
+# #Python coding challenge **12
+# # A "Perfect Number" is a number in which the sum of its factors (excluding itself) are equal to itself.
+# Write a function that can verify if the given integer n is a perfect number, 
+# and return True if it is, or return False if not.
+# Example
+# n = 28 has the following factors: 1, 2, 4, 7, 14
+# 1 + 2 + 4 + 7 + 14 = 28 therefore n = 28 is a perfect number, so you should return True.
+# n = 25 has the following factors: 1, 5, 25
+# 1 + 5 = 6 therefore n = 25 is not a perfect number, so you should return False.
+
+print("...Finding a perfect number...")
