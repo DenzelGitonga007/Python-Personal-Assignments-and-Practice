06adadb5110bca7b3a62dab4843502e5aa31f336
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-26 11:19:32 +0300

    round one done

--- 21_Days_of_Py/card_game.py
+++ 21_Days_of_Py/card_game.py
@@ -25,12 +25,117 @@
 # Tip
 # Players can actually play DUMB moves, especially Grace and Linus.
 
+import random # For random selection
+
 # Function to play
 def card_game(Ed, Grace, Linus):
     """...Playing now..."""
-    print("")
+    print()
     print("...Let the game begin...")
-    
+    print("...Round One...")
+    # To set who begins, then remove the 0
+    if 0 in Ed:
+        print("Ed begins because he has the 0: ", Ed)
+        Ed.remove(0)
+        # Add the score onto the dictionary
+        round_one_scores = {
+            "Ed" : 0
+        }
+        print("He remains with: ",Ed)
+        print()
+    elif 0 in Grace:
+        print("Grace begins because she has the 0: ", Grace)
+        # Add the score onto the dictionary
+        round_one_scores = {
+            "Grace" : 0
+        }
+        Grace.remove(0)
+        print("She remains with: ", Grace)
+        print()
+    else:
+        print("Linus begins because he has the 0: ", Linus)
+        # Add the score onto the dictionary
+        round_one_scores = {
+            "Linus" : 0
+        }
+        Linus.remove(0)
+        print("He remains with: ", Linus)
+        print()
+    # The other players to play, but not the one who started with the 0
+    if 0 not in Ed and len(Ed) != 3:
+        print("Ed playing... ... ...")
+        # Randomly select a card
+        Ed_round_one = random.sample(Ed, 1)
+        # Remove the card from the Ed's list, as this is how the game is played
+        Ed.remove(Ed_round_one[0]) # because Ed_round_one is a list
+        print("He plays with card {} and remains with {}".format(Ed_round_one, Ed))
+        # Update the score onto the dictionary
+        round_one_scores["Ed"] = Ed_round_one[0]
+        print()
+    else:
+        print("Grace or Linus to play next...")
+        print()
+    if 0 not in Grace and len(Grace) != 3:
+        print("Grace playing... ... ...")
+        # Randomly select a card
+        Grace_round_one = random.sample(Grace, 1)
+        # Remove the card from Grace's list, as this is how the game is played
+        Grace.remove(Grace_round_one[0]) # because Grace_round_one is a list
+        print("She plays with card {} and remains with {}".format(Grace_round_one, Grace))
+        # Update the score onto the dictionary
+        round_one_scores["Grace"] = Grace_round_one[0]
+        print()
+    else:
+        print("Linus go next")
+
+    if 0 not in Linus and len(Linus) != 3:
+        print("Linus playing... ... ...")
+        # Randomly select a card
+        Linus_round_one = random.sample(Linus, 1)
+        # Remove the card from Linus' list, as this is how the game is played
+        Linus.remove(Linus_round_one[0]) # because Linus_round_one is a list
+        print("He plays with card {} and remains with {}".format(Linus_round_one, Linus))
+        # Update the score onto the dictionary
+        round_one_scores["Linus"] = Linus_round_one[0]
+        print()
+        print("...Round One Closed...")
+    else:
+        print("...Round One Closed...")
+    # View the scoreboard as the round_one_scores
+    print()
+    print("...Check out the scores now...")
+    print("The scoreboard: ", round_one_scores)
+    # Getting the winner of the first round
+    round_one_winner = max(round_one_scores, key=round_one_scores.get)
+    print("And round one winner is {} with {} points".format(round_one_winner, round_one_scores.get(round_one_winner)))
+    print()
+    print("...Round Two...")
+    print("Cards remaining...")
+    print("Ed: ", Ed)
+    print("Grace: ",Grace)
+    print("Linus: ", Linus)
+    print()
+    # The winner of the first round plays next
+    if round_one_winner == "Ed":
+        print("Ed plays first because he won the first round")
+        Ed_round_two = random.sample(Ed, 1)
+        # Remove the card
+        Ed.remove(Ed_round_two[0]) # Because Ed_round_two is a list
+        print("Ed plays with card {} and remains with {} cards".format(Ed_round_two, Ed))
+    elif round_one_winner == "Grace":
+        print("Grace plays first because she won the first round")
+        Grace_round_two = random.sample(Grace, 1)
+        # Remove the card
+        Grace.remove(Grace_round_two[0]) # Because Grace_round_two is a list
+        print("Grace plays with card {} and remains with {} cards".format(Grace_round_two, Grace))
+    else:
+        print("Linus plays first because he won the first round")
+        Linus_round_two = random.sample(Linus, 1)
+        # Remove the card
+        Linus.remove(Linus_round_two[0]) # Because Linus_round_two is a list
+        print("Linus plays with card {} and remains with {} cards".format(Linus_round_two, Linus))
+
+
 print("...Card Game...")
 print()
 print("Players are: Ed, Grace, Linus")
@@ -40,7 +145,6 @@ cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 print("The cards available are: ", cards)
 print()
 # Randomly select the cards and assign them to the players
-import random
 Ed = sorted(random.sample(cards, 4))
 # To ensure that cards assigned to Ed are not in the list anymore
 for Ed_cards in Ed:
@@ -52,14 +156,14 @@ Grace = sorted(random.sample(cards, 4))
 # To ensure that cards assigned to Grace are not in the list anymore
 for Grace_cards in Grace:
     cards.remove(Grace_cards)
-print("After giving Grace {} cards, the cards that remain are: {}".format(Grace, cards))
+print("Grace takes {} cards, and the cards that remain are: {}".format(Grace, cards))
 print()
 # Give to Linus
 Linus = sorted(random.sample(cards, 4))
 # To ensure that cards assigned to Linus are not in the list anymore
 for Linus_cards in Linus:
     cards.remove(Linus_cards)
-print("After giving Ed {} cards, the cards that remain are: {}".format(Linus, cards))
+print("Linus takes the remaining cards: {}".format(Linus))
 print()
 print("The players are now ready to play: ")
 print()
@@ -68,6 +172,11 @@ print()
 print("Grace: ",Grace)
 print()
 print("Linus: ",Linus)
+# Declare the dictionaries for scores; player, score pairs
+round_one_scores = {}
+round_two_scores = {}
+round_three_scores = {}
+round_four_scores = {}
 card_game(Ed, Grace, Linus)
 
 
