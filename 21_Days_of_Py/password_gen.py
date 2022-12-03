c396f1e746c3980fe14efdd740e5e6d740392fa5
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-03 10:38:07 +0300

    generating a weak password

--- 21_Days_of_Py/password_gen.py
+++ 21_Days_of_Py/password_gen.py
@@ -6,21 +6,42 @@
 # Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 
 # Start
-# print("...Password generator...")
-# # Ask the user how strong they want their password to be
-# # Also, only accept string values
+import random # For the random selection
+print("...Password generator...")
+# Ask the user how strong they want their password to be
 while True:
     try:
         level = (input("How do you wish your password to be: weak or strong? "))
-        print("You entered: ", level)
-        if not level.isalpha() and level != "weak" or level != "strong":
-            print("Please enter texts only, either strong or weak: ")
+        if not level.isalpha():
+            raise ValueError # Handled by the except function
+        if level != "weak" and level != "strong":
+            print("Please select either strong or weak, you entered: ", level)
         else:
             break
     except:
-        pass
-print("For a {} password".format(level))
+        print("Oops! Sorry, only text values allowed, you entered: ", level)
+print("For a {} password:".format(level))
 # For weak password
+if level == "weak":
+    # Get the user's name to have a list to pick two names from
+    name = []
+    number_of_names = 3
+    print("Enter any three names so that we can randomly select two of them, and generate a password from these names")
+    print("Hint: Type the name and press enter to type the next...")
+    for names in range(0, number_of_names):
+        # Get the names now
+        names = input("")
+        # Append the names onto the name list
+        name.append(names)
+    random_name = random.sample(name, 2)
+    print("From {}, we have selected {} and {}".format(name, random_name[0], random_name[1]))
+    # Now join the random_name to form the password
+    print("And your password is: ","".join(random_name))
+
+
+else:
+    print("Strong strong...")
+
 
 
 
