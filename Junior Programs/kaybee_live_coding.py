4219fe98da7ac9ab3e099698aa07d40919d766e8
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-05 14:49:21 +0300

    mean

--- Junior Programs/kaybee_live_coding.py
+++ Junior Programs/kaybee_live_coding.py
@@ -2,3 +2,13 @@
 # 1. Write a function to calculate mean
 # 2. Write a function to calculate variance
 # 3. Calculate standard deviation
+
+# Start
+import statistics
+# Declare the Sample_data
+sample_data=[22, 28,33,26,39,15,19]
+# Start the function
+def calculations(sample_data):
+    print("The mean is:",statistics.mean(sample_data))
+# Call the function
+calculations(sample_data)
