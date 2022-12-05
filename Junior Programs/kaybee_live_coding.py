b2110ebe46584d4e353048a217cf8f9327c29425
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-05 14:56:36 +0300

    variance and standard deviation

--- Junior Programs/kaybee_live_coding.py
+++ Junior Programs/kaybee_live_coding.py
@@ -6,9 +6,14 @@
 # Start
 import statistics
 # Declare the Sample_data
-sample_data=[22, 28,33,26,39,15,19]
+sample_data=[22,28,33,26,39,15,19]
 # Start the function
 def calculations(sample_data):
+    # Calculate the mean
     print("The mean is:",statistics.mean(sample_data))
+    # Calculate the variance
+    print("The variance is:",statistics.variance(sample_data))
+    # Calculate the standard deviation
+    print("The standard deviation:",statistics.stdev(sample_data))
 # Call the function
 calculations(sample_data)
