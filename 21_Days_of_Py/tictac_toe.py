252277537701bf2b8c5478469e3a7a57787a7e77
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-19 20:56:31 +0300

    Error handling in place

--- 21_Days_of_Py/tictac_toe.py
+++ 21_Days_of_Py/tictac_toe.py
@@ -5,3 +5,21 @@
 # (8x8 for chess, 19x19 for Go, and many more).
 # - Ask the user what size game board they want to draw, 
 # and draw it for them to the screen using Python’s print statement.
+
+# Get the size of the board from the user
+while True:
+    try:
+        # Accept only integer values
+        size = int(input("Enter the size of the board you wish to use:"))
+        # The value should not be less than zero
+        try:
+            if size <= 0:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print("Size cannot be less than zero or zero itself")
+    except:
+        print("Please enter a numerical value only")
+
+    
