8f5c7afe6a6523e7dcad567bbc541fccb868cf00
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-19 20:33:12 +0300

    Tictac toe

---
+++ 21_Days_of_Py/tictac_toe.py
@@ -0,0 +1,7 @@
+#*15*
+#TIC TAC TOE GAME
+## PART ONE -- Draw the tic tac toe board
+# - Create a 3x3 board (like in tic tac toe). Obviously, they come in many other sizes 
+# (8x8 for chess, 19x19 for Go, and many more).
+# - Ask the user what size game board they want to draw, 
+# and draw it for them to the screen using Python’s print statement.
