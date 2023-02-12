0c04e01c338b0972ad88a0d68406e3f4ccdc47ea
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-12 19:55:55 +0300

    db class

--- Junior_Programs/python_with_db/py_with_db.py
+++ Junior_Programs/python_with_db/py_with_db.py
@@ -1,1 +1,20 @@
 import mysql.connector
+
+# Class for DB
+class Database:
+    my_db = my_cursor = None
+
+    def __init__(self):
+        global my_db, my_cursor
+        my_db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="hospital"
+        )
+        my_cursor = my_db.cursor()
+
+    def __del__(self):
+        #     my_db.commit() initial
+        my_db.commit()
+      
