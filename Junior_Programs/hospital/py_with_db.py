96c51e64d2c106125467a53db9f05dc6206e7e57
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-14 19:50:04 +0300

    trying to insert records

--- Junior_Programs/hospital/py_with_db.py
+++ Junior_Programs/hospital/py_with_db.py
@@ -29,20 +29,47 @@
 
 # THE ABOVE CODE CONFIRMS THAT THE DB IS ACTUALLY THERE
 
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="",
+#   database="hospital"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE TABLE daktari (FIRST_NAME VARCHAR(255), LAST_NAME VARCHAR(255), AGE INT(3), GENDER VARCHAR(8), SPECIALITY VARCHAR(255), INCOME decimal(15,2))")
+
+# THE ABOVE CODE CREATES THE TABLE
+
+# INSERTING THE DATA
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="hospital"
-)
+#establishing the connection
+conn = mysql.connector.connect(
+   user='root', password='', host='127.0.0.1', database='hospital')
 
-mycursor = mydb.cursor()
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
 
-mycursor.execute("CREATE TABLE daktari (FIRST_NAME VARCHAR(255), LAST_NAME VARCHAR(255), AGE INT(3), GENDER VARCHAR(8), SPECIALITY VARCHAR(255), INCOME decimal(15,2))")
+# Preparing SQL query to INSERT a record into the database.
+sql = """INSERT INTO daktari(FIRST_NAME, LAST_NAME, AGE, GENDER, SPECIALITY,INCOME)VALUES('Coronasias', 'Onyango', 40, 'MLAE', 80000)"""
 
+try:
+   # Executing the SQL command
+   cursor.execute(sql)
 
+   # Commit your changes in the database
+   conn.commit()
+
+except:
+   # Rolling back in case of error
+   conn.rollback()
+
+# Closing the connection
+conn.close()
 
 
 
